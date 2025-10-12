7308a1399ddccb189f37d2c220c9692b77be535d
George Lugo <machinelearner2334@gmail.com>
2025-10-12 01:34:43 +0000

    fix: made fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    runtime: "edge",
-  },
+  experimental: {},
 };
 
 export default nextConfig;
